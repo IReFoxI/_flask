35f68ab5422bdff189745b2ae0be08d0e69aa69a
IReFoxI <ra.mailmaster@gmail.com>
2020-07-16 01:11:00 +0300

    added function: add_message

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from flask import Flask, escape, request
+from collections import namedtuple
+from flask import Flask, request, render_template, redirect, url_for
+
 
 app = Flask(__name__)
 
+Message = namedtuple('Message', 'text tag')
+messages =[]
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
-    return 'Index Page'
+    return render_template('index.html')
 
-@app.route('/hello')
+
+@app.route('/hello', methods=['GET'])
 def hello():
-    return 'Hello, Worlds'
+    return render_template('hello.html', messages=messages)
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+    messages.append(Message(text, tag))
+
+    return redirect(url_for('hello'))
+
 # $ env FLASK_APP=hello.py flask run
 #  * Serving Flask app "hello"
 #  * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
